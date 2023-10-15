2e42d5bb3fbe83b03819add52db28826a82ecc98
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-10-15 21:28:58 +0500

    created ripplecard

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
               <RippleCard
                 key={post?._id}
                 id={post?._id}
-                currentUserId={user?.id}
+                currentUserId={user?.id || ''}
                 parentId={post?.parentId}
                 content={post.text}
                 author={post?.author}
