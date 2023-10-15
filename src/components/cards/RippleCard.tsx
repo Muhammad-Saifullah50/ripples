9076faa31437b1a23412030619e33f50ab684f7b
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-10-15 16:45:40 +0500

    started ripple card

---
+++ src/components/cards/RippleCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RippleCard = () => {
+  return (
+    <div>RippleCard</div>
+  )
+}
+
+export default RippleCard
--- src/lib/actions/ripple.actions.ts
+++ src/lib/actions/ripple.actions.ts
@@ -34,3 +34,36 @@ export const createRipple = async ({ text, author, communityId, path }: Params) 
     }
 
 }
+
+export const fetchRipples = async (pageNumber = 1, pageSize = 20) => {
+    try {
+        connectToDB()
+        // calculate the number of posts to skip
+        const skipAmount = (pageNumber - 1) * pageSize
+
+        // we have to fetch posts having no parents(meaning they are original posts nor comments)
+
+        const postsQuery = Ripple.find({ parentId: { $in: [null, undefined] } })
+            .sort({ createdAt: 'desc' })
+            .skip(skipAmount)
+            .limit(pageSize)
+            .populate({ path: 'author', model: User })
+            .populate({
+                path: 'children',
+                populate: {
+                    path: 'author',
+                    model: User,
+                    select: '_id name parentId image'
+                }
+            });
+
+        const totalPostsCount = await Ripple.countDocuments({ parentId: { $in: [null, undefined] } });
+
+        const posts = await postsQuery.exec();
+
+        const isNext = totalPostsCount > skipAmount + posts.length
+        return { posts, isNext }
+    } catch (error) {
+
+    }
+}
