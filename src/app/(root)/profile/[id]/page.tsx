a1a524775eb787aba0d5174e1cf52dc7517e63ae
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-10-17 21:58:41 +0500

    completed profile header

--- src/app/(root)/profile/[id]/page.tsx
+++ src/app/(root)/profile/[id]/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { PostRipple } from "@/components/forms"
+import { ProfileHeader } from "@/components/shared"
+import { fetchUser } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from 'next/navigation'
 
-const ProfilePage = () => {
+const ProfilePage = async ({ params }: { params: { id: string } }) => {
+
+  const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(params.id)
+
+  if (!userInfo?.onboarded) redirect('/onboarding')
+
   return (
-    <div>ProfilePage</div>
+    <section>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.image}
+        bio={userInfo.bio}
+      />
+
+      <div className="mt-9">
+
+      </div>
+    </section>
   )
 }
 
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,10 +2,12 @@ import Topbar from "./Topbar";
 import Bottombar from "./Bottombar";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "./RightSidebar";
+import ProfileHeader from "./ProfileHeader";
 
 export {
     Topbar,
     Bottombar,
     LeftSidebar,
-    RightSidebar
+    RightSidebar,
+    ProfileHeader
 }
