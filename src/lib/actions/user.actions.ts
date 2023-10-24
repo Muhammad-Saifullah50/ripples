ce64ebfcf35bd50e4b3acf4aa3367e155fc67634
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-10-24 08:49:53 +0500

    competed search page

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache"
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
 import Ripple from "../models/ripple.model"
+import { FilterQuery, SortOrder } from "mongoose"
 
 interface Params {
     userId: string,
@@ -72,16 +73,58 @@ export const fetchUserPosts = async (userId: string) => {
                     })
                 })
             })
-            return ripples
+        return ripples
     } catch (error: any) {
-throw new Error(`Failed to fetch posts ${error?.message}`)
+        throw new Error(`Failed to fetch posts ${error?.message}`)
     }
 }
 
-export const fetchUsers = async () => {
+export const fetchUsers = async ({
+    userId,
+    searchString = '',
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = 'desc'
+}: {
+    userId: string,
+    searchString?: string,
+    pageNumber?: number,
+    pageSize?: number,
+    sortBy?: SortOrder
+}) => {
     try {
         connectToDB()
-    } catch (error) {
-        
+
+        const skipAmount = (pageNumber - 1) * pageSize
+
+        const regex = new RegExp(searchString, 'i')
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId }
+        }
+
+        if (searchString.trim() !== '') {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } },
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy }
+
+        const UserQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize)
+
+        const totalUsersCount = await User.countDocuments(query)
+
+        const users = await UserQuery.exec()
+
+        const isNext = totalUsersCount > skipAmount + users.length
+
+        return { users, isNext }
+    } catch (error: any) {
+        throw new Error(`Failed to fetch users ${error?.message}`)
     }
 }
