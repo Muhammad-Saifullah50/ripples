c94aa68d4c56f9f342febbc37ccc35c6f1d8dc5b
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-11-15 11:56:57 +0500

    corrected redirection errors

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -9,7 +9,7 @@ const Onboarding = async () => {
   const user = await currentUser()
   if (!user) return null
   const userInfo = await fetchUser(user?.id)
-  if (!userInfo?.onboarded) redirect('/')
+  if (userInfo?.onboarded) redirect('/')
 
   const userData = {
     id: user?.id,
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,21 @@
 import RippleCard from "@/components/cards/RippleCard"
 import Pagination from "@/components/shared/Pagination"
 import { fetchPosts } from "@/lib/actions/ripple.actions"
+import { fetchUser } from "@/lib/actions/user.actions"
 import { currentUser } from '@clerk/nextjs'
+import { redirect } from "next/navigation"
 
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
-  const result = await fetchPosts(searchParams?.page ? +searchParams?.page : 1, 20)
+
   const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect('/onboarding')
+  const result = await fetchPosts(searchParams?.page ? +searchParams?.page : 1, 20)
+
+
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
